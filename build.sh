1ac0048e0039aaf97c547218030e28c18a13d1a4
wangsrGit119 <l>
2022-04-07 16:20:17 +0800

    fix: update script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ version=`date +%Y%m%d`
 
 echo "build time $version , build version $version"
 docker rmi $version
-docker build -t janus-webrtc-gateway-docker:$version .
+docker build -t sucwangsr/janus-webrtc-gateway-docker:$version .
--- push.sh
+++ push.sh
@@ -4,5 +4,5 @@ version=`date +%Y%m%d`
 
 echo "push time $version , push version $version"
 docker login -u $1 -p $2
-docker push janus-webrtc-gateway-docker:$version
+docker push sucwangsr/janus-webrtc-gateway-docker:$version
 docker logout
