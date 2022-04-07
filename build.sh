e204455ba2b0f6b34686899f0a05a47d4d8530f7
wangsrGit119 <l>
2022-04-07 16:02:55 +0800

    fix: update script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ version=`date +%Y%m%d`
 
 echo "build time $version , build version $version"
 docker rmi $version
-docker build -t docker-webrtc-gateway:$version .
+docker build -t janus-webrtc-gateway-docker:$version .
